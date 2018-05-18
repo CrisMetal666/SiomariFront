306cd56e6480d27fe22dfc9c7890f9d23abfa5ae
CrisMetal666 <cristian.giraldo.carmen@gmail.com>
2018-05-18 09:59:43 -0500

    Consulta de entregas mejorado, proceso de manejo de agua terminado

--- src/app/_service/manejo-agua.service.ts
+++ src/app/_service/manejo-agua.service.ts
@@ -18,4 +18,43 @@ export class ManejoAguaService {
     return this.http.post<ManejoAgua>(this.url, manejoAgua, { observe: 'response' });
   }
 
+  /**
+	 * se calculara la eficiencia, lamina neta, lamina bruta de un canal, seccion,
+	 * zona, unidad en el rango de fecha especificado
+	 * 
+	 * @param fecha1
+	 *            fecha inferior
+	 * @param fecha2
+	 *            fecha superior
+	 * @param id
+	 *            id de la unidad, zona, seccion o canal
+	 * @param tipo
+	 *            dira si se trata de unidad, zona, seccion o canal. 1 = unidad, 2 =
+	 *            zona, 3 = seccion, 4 = canal
+	 * @return caculo ficiencia, lamina neta, lamina bruta en cada fecha que hubo
+	 *         registro dentro del rango
+	 */
+  calcularLanLamEfic(fecha1: Date, fecha2: Date, id: number, tipo: number) {
+
+    let txtFecha1: string = this.dateToString(fecha1);
+    let txtFecha2: string = this.dateToString(fecha2);
+
+    return this.http.get<Array<Array<number>>>(`${this.url}calcularLanLamEfic?fecha1=${txtFecha1}&fecha2=${txtFecha2}&id=${id}&tipo=${tipo}`);
+  }
+
+  /**
+   * se devuleve la fecha en formato yyyy-mm-dd
+   * @param fecha fecha a parcear
+   * @return fecha en texto
+   */
+  private dateToString(fecha: Date): string {
+
+    let dia: number = fecha.getDate();
+    let mes: number = fecha.getMonth() + 1;
+    let diaString: string = dia < 10 ? '0' + dia : '' + dia;
+    let mesString: string = mes < 10 ? '0' + mes : '' + mes;
+
+    return `${fecha.getFullYear()}-${mesString}-${diaString}`;
+  }
+
 }
--- src/app/componentes/manejo-agua-grafica/manejo-agua-grafica.component.ts
+++ src/app/componentes/manejo-agua-grafica/manejo-agua-grafica.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { esLocale } from 'ngx-bootstrap/locale';
+import { BsDatepickerConfig, BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { CompleterData, CompleterService, CompleterItem } from 'ng2-completer';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { CanalService } from '../../_service/canal.service';
+import { ManejoAguaService } from '../../_service/manejo-agua.service';
+import { UnidadService } from '../../_service/unidad.service';
+import { ZonaService } from '../../_service/zona.service';
+import { SeccionService } from '../../_service/seccion.service';
 
 @Component({
   selector: 'app-manejo-agua-grafica',
@@ -7,40 +17,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManejoAguaGraficaComponent implements OnInit {
 
-  //plaholder del autocompleter
-  public textCompleter: string;
   // rango de fecha seleccionado
   public fecha: Date[];
+  //autocompleter
+  public dataCanal: CompleterData;
+  public dataUnidad: CompleterData;
+  public dataSeccion: CompleterData;
+  public dataZona: CompleterData;
+  //id seleccionado en el autocompleter
+  public idCanal: number;
+  public idUnidad: number;
+  public idZona: number;
+  public idSeccion: number;
+  public sCanal: string;
+  public sUnidad: string;
+  public sZona: string;
+  public sSeccion: string;
+  //segun el vlaor numerico, se mostrara un mensaje al usuario
+  public estado: number;
   // lineChart
-  public lineChartData: Array<any> = [
-    { data: [30,40,50,60,54,56,58,58,60,62,62,65,60,62,62,65,65,65,62,56,70,66,33,55,78,76,77,54,56,78], label: 'Eficiencia' },
-    { data: [55,40,30,20,25,20,18,18,15,15,13,16,15,15,13,13,13,15,16,20,15,20,21,22,15,14,16,16,16,15], label: 'Lam' },
-    { data: [45,30,20,10,15,10,8,8,5,5,3,6,5,5,3,3,3,5,6,10,5,10,11,12,5,4,6,6,6,5], label: 'Lan' }
-  ];
-  public lineChartLabels: Array<any> = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  public lineChartData: Array<any> = [];
+  public lineChartLabels: Array<any> = [];
   public lineChartOptions: any = {
     responsive: true
   };
   public lineChartColors: Array<any> = [
-    { 
+    {
       backgroundColor: 'rgba(148,159,177,0)',
-      borderColor: '#FF8000',
+      borderColor: '#c62828',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
-    { 
+    {
       backgroundColor: 'rgba(148,159,177,0)',
-      borderColor: '#CDDC39',
+      borderColor: '#0d47a1',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
-    { 
+    {
       backgroundColor: 'rgba(148,159,177,0)',
-      borderColor: '#009688',
+      borderColor: '#90caf9',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
@@ -50,13 +70,229 @@ export class ManejoAguaGraficaComponent implements OnInit {
   public lineChartLegend: boolean = true;
   public lineChartType: string = 'line';
   public mostrarGrafica: boolean;
+  /*
+  * determinara si se graficara los datos de una canal, seccion, zona o unidad
+  * 1 = unidad
+  * 2 = zona
+  * 3 = seccion
+  * 4 = canal
+  * por defecto sera 4
+  */
+  public tipoGrafico: number;
+  // dira si se han seleccionado todos los autocompleter
+  public valido: boolean;
 
-  constructor() { 
+  constructor(
+    private completerService: CompleterService,
+    private spinnerService: Ng4LoadingSpinnerService,
+    private _localeService: BsLocaleService,
+    private canalService: CanalService,
+    private manejoAguaService: ManejoAguaService,
+    private unidadService: UnidadService,
+    private zonaService: ZonaService,
+    private seccionService: SeccionService
+  ) {
     this.mostrarGrafica = false;
-    this.textCompleter = 'Buscar canal';
+    this.tipoGrafico = 4;
+    this.valido = false;
   }
 
   ngOnInit() {
+    //definimos el idioma del datepicker
+    defineLocale('es', esLocale);
+    this._localeService.use('es');
+    //inicializamos el autocompleter
+    this.dataCanal = this.completerService.remote(this.canalService.urlListarPorNombreOCodigo, 'nombre,codigo', 'nombre');
+
+    // inicializamos el auto-completer de unidad
+    this.spinnerService.show();
+
+    this.unidadService.listarTodos().subscribe(res => {
+
+      this.dataUnidad = this.completerService.local(res, 'nombre', 'nombre');
+      this.spinnerService.hide();
+
+    }, err => {
+      this.estado = 0;
+      this.spinnerService.hide();
+    });
+  }
+
+  onSourceSelect(selected: CompleterItem) {
+
+    this.idCanal = 0;
+    this.valido = false;
+
+    if (selected) {
+      this.idCanal = selected.originalObject.id;
+      this.valido = true;
+    }
+  }
+
+  onUnidadSelect(selected: CompleterItem) {
+
+    this.idUnidad = 0;
+    this.valido = false;
+    //limpiamos los autocompleter en cascada
+    this.idZona = 0;
+    this.sZona = '';
+    this.idSeccion = 0;
+    this.sSeccion = '';
+
+    if (selected) {
+
+      this.idUnidad = selected.originalObject.id;
+      this.valido = true;
+
+      // buscamos las zonas de la unidad solo si esta seleccionado zona o seccion (radio boton)
+      if (this.tipoGrafico != 1) {
+
+        this.valido = false;
+
+        this.spinnerService.show();
+
+        this.zonaService.buscarPorUnidadId(this.idUnidad).subscribe(res => {
+
+          this.dataZona = this.completerService.local(res, 'nombre', 'nombre');
+          this.spinnerService.hide();
+
+        }, err => {
+          this.estado = 0;
+          this.spinnerService.hide();
+        });
+      }
+    }
+  }
+
+  onZonaSelect(selected: CompleterItem) {
+
+    this.idZona = 0;
+    this.valido = false;
+
+    //limpiamos los valores de la seccion
+    this.idSeccion = 0;
+    this.sSeccion = '';
+
+    if (selected) {
+
+      this.idZona = selected.originalObject.id;
+      this.valido = true;
+
+      //se buscaran las secciones si esta seleccionado la seccion (radio boton)
+      if (this.tipoGrafico == 3) {
+
+        this.spinnerService.show();
+
+        this.valido = false;
+
+        this.seccionService.buscarPorZonaId(this.idZona).subscribe(res => {
+
+          this.dataSeccion = this.completerService.local(res, 'nombre', 'nombre');
+          this.spinnerService.hide();
+
+        }, err => {
+          this.estado = 0;
+          this.spinnerService.hide();
+        })
+      }
+    }
+  }
+
+  onSeccionSelect(selected: CompleterItem) {
+
+    this.idSeccion = 0;
+    this.valido = false;
+
+    if (selected) {
+      this.idSeccion = selected.originalObject.id;
+      this.valido = true;
+    }
+  }
+
+  /*
+   * al seleccionar un item del radio button para mostrar los autocompleter
+   * segun el item seleccionado 
+   */
+  onClickChoose(tipo: number, form) {
+    this.tipoGrafico = tipo;
+    //reinisiamos los id de los autocompleter
+    this.idUnidad = 0;
+    this.idSeccion = 0;
+    this.idZona = 0;
+    this.idCanal = 0;
+    this.sUnidad = '';
+    this.sSeccion = '';
+    this.sZona = '';
+    this.sCanal = '';
+
+    //reseteamos el form donde estan los autocompleter
+    form.reset();
+  }
+
+  consultar() {
+
+    this.spinnerService.show();
+    //necesario para que la grafica se refresque
+    this.mostrarGrafica = false;
+
+    // guardaremos el id segun el tipo que se escoja 
+    let id = 0;
+
+    if (this.tipoGrafico == 1) {
+
+      id = this.idUnidad;
+
+    } else if (this.tipoGrafico == 2) {
+
+      id = this.idZona;
+
+    } else if (this.tipoGrafico == 3) {
+
+      id = this.idSeccion;
+
+    } else if (this.tipoGrafico == 4) {
+
+      id = this.idCanal;
+
+    } else {
+      return;
+    }
+
+    this.manejoAguaService.calcularLanLamEfic(this.fecha[0], this.fecha[1], id,
+      this.tipoGrafico).subscribe(res => {
+
+        this.estado = undefined;
+
+        //si la lista esta vacia mostramos un mensaje al usuario
+        if (res.length == 0) {
+          this.estado = 2;
+          this.mostrarGrafica = false;
+          this.spinnerService.hide();
+          return;
+        }
+
+        //valores del eje x que son igual a la cantidad de posiciones en el array
+        let ejeX: Array<number> = [];
+
+        for (let i = 1; i <= res[0].length; i++) {
+          ejeX.push(i);
+        }
+
+        this.lineChartData = [
+          { data: res[2], label: 'Eficiencia' },
+          { data: res[1], label: 'Lam' },
+          { data: res[0], label: 'Ln' },
+        ];
+
+        this.lineChartLabels = ejeX;
+
+        this.mostrarGrafica = true;
+        this.spinnerService.hide();
+
+      }, err => {
+        this.estado = 0;
+        this.spinnerService.hide();
+      });
   }
 
 }
